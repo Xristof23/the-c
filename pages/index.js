79c4eac72ce85b4921d67f39a43ec95583babd35
Christof <userpattern@gmail.com>
2025-01-03 12:42:51 +0100

    corrections in content

--- pages/index.js
+++ pages/index.js
@@ -12,24 +12,32 @@ export default function HomePage() {
       <Headline1>The C</Headline1>
       <StandardParagraph>
         Everything is about life. Everything is about Death. She defines how we live. At first sight She can only destroy. <br/>
-        Yes, she can not create life or can she? In nature Every Death is another beginning. Some mushrooms only grow from chared tree trunks, worms will feed on my cold dead body some (hopefully still very distant) day <br />
+        Yes, She can not create life or can She? In nature, every death is another beginning. Some mushrooms only grow from charred tree trunks,
+        worms will feed on my cold dead body some (hopefully still very distant) day. <br />
         Creating art is a way to respect death while defying Her.
-        Actually it might one of the very few ways to do that, and the only way which is under your personal control.
-        After thousands of years people still read Homer, people making movies - right now, the oddysey is under way. Homer&apos;s impact may be not as big as it used to be ...
-        but hey, if millions are  it is still very big. 
-        The C will never rival the great poets, but The C creates musical art. At least he tries. So The C craves a glimpse of eternity like so many otherhumen beings do.
+        Actually it might one of the very few ways to do that, and the only way which is under your personal control.<br/>
+        After thousands of years people still read Homer, people making movies - right now, the oddysey is in the making directed bei Christopher Nolan. 
+        Homer&apos;s impact may be not as huge as it used to be ... but hey, if millions are invested in a movie about a story
+        that&apos;s more than 2500 years old -  it is still very big.
+        The C will never rival the great poets, but The C creates musical art to persist in the memories of people listing to the music - at least that ist the goal.
+        If only a few or hundreds or thousands will listen and remember does not matter that much ... better to leave a piece that a few hold dear than something forgettable known by the masses for a short time. <br />
+        So The C craves a glimpse of eternity like so many other human beings do.
 
 
       </StandardParagraph>
       <Headline1>About the music</Headline1>
       <StandardParagraph>
         After all the pseudo philosophical gibberish in the paragraph above, here is some more down to earth information about the projects style. <br />
-        There are guitars, they will be overdriven, distorted or fuzzy, seldom clean, there is bass, sometimes clean, there are drums, sometimes other instruments.
-        Theres a voice, it is emotional and some would call teh style authentic but thats a difficult word for me.
-        So the C works influences Hard Rock, Psychedelic, Noise, Punk and Pop into an original mix. The C is a child of the 90s, but attempts to create a kind of presnt and timeless music.<br />earlo
+        There are guitars, they are overdriven, distorted or fuzzy, seldom clean, there is bass, sometimes clean, there are drums, sometimes other instruments. <br />
+        There&apos;s a voice, it is mostly emotional and some would call the style authentic but that&apos;s a difficult word for me.
+        The voice talks about dark themes like braking relationships, everyday frustration,
+        death and the corruption of mankind but &quot;As long as there&apos;s life there is hope &quot; (Tolkien),
+        so there are some slightly optimistic bits as well. <br />
+        So the C works influences Hard Rock, Psychedelic, Noise, Punk and Pop into
+        an original mix that is tending to linger on the dark side.
+        The C is a musical child of the 90s return to rock (called grunge at the time), but attempts to create a kind of present and timeless musical style .<br />
         
-        Kyuss would not have mead ewithout Balck Sabbath ...
-        Dark themes but &quot;As long as there&apos;s life there is hope &quot;
+    
       </StandardParagraph>
     </MainSection>
   );

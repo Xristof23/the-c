ee37fe46d36dd1dc2add92a235a65d6b0a5482c9
Christof <userpattern@gmail.com>
2025-01-04 17:28:18 +0100

    add test mp3 snippet

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,27 @@
-import { ButtonContainer, FlexRowWrapper, StandardButton, } from "@/sharedStyledComponents"
+import {
+  ButtonContainer,
+  FlexRowWrapper,
+  StandardButton,
+} from "@/sharedStyledComponents";
 
 export default function Header() {
-    return (
+  return (
+    <FlexRowWrapper>
+      <ButtonContainer>
+        <StandardButton onClick={() => console.log("Play music pressed")}>
+          play music{" "}
+        </StandardButton>{" "}
+        <StandardButton onClick={() => console.log("sound fx")}>
+          sound fx{" "}
+        </StandardButton>
+      </ButtonContainer>
+      <figure>
+        <figcaption>Listen to The C</figcaption>
         <FlexRowWrapper>
-            <ButtonContainer>
-        <StandardButton onClick={()=> console.log("Play music pressed")}>play music </StandardButton> <StandardButton onClick={()=> console.log("sound fx")}>sound fx </StandardButton>
-            </ButtonContainer>
-            <figure>
-                <figcaption>Listen to The C</figcaption>
-                <FlexRowWrapper>
-                <audio controls src="/media/cc0-audio/t-rex-roar.mp3"></audio>
-                    {/* <a href="/sounds/t-rex-roar.mp3"> Download audio </a> */}
-                    </FlexRowWrapper>
-            </figure>
-
+          <audio controls src="/public/tracks/SimpleSongPart.mp3"></audio>
+          {/* <a href="/sounds/t-rex-roar.mp3"> Download audio </a> */}
         </FlexRowWrapper>
-    )
+      </figure>
+    </FlexRowWrapper>
+  );
 }

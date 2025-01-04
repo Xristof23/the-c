da3e7f11799b80575404b76245304b26e5c87130
Christof <userpattern@gmail.com>
2025-01-04 17:18:51 +0100

    now with Header

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import { ButtonContainer, FlexRowWrapper, StandardButton, } from "@/sharedStyledComponents"
+
+export default function Header() {
+    return (
+        <FlexRowWrapper>
+            <ButtonContainer>
+        <StandardButton onClick={()=> console.log("Play music pressed")}>play music </StandardButton> <StandardButton onClick={()=> console.log("sound fx")}>sound fx </StandardButton>
+            </ButtonContainer>
+            <figure>
+                <figcaption>Listen to The C</figcaption>
+                <FlexRowWrapper>
+                <audio controls src="/media/cc0-audio/t-rex-roar.mp3"></audio>
+                    {/* <a href="/sounds/t-rex-roar.mp3"> Download audio </a> */}
+                    </FlexRowWrapper>
+            </figure>
+
+        </FlexRowWrapper>
+    )
+}
